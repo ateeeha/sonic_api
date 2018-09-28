53a9f009dbc987a51bf6c1df71d31d2d4ec800fd
ateeeha <ateeeha@gmail.com>
2018-09-28 15:48:33 +0700

    first commit

---
+++ application/controllers/User.php
@@ -0,0 +1,84 @@
+<?php
+
+require APPPATH . 'libraries/REST_Controller.php';
+
+class User extends REST_Controller{
+
+  // construct
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('user_model');
+  }
+
+  // method index untuk menampilkan semua data person menggunakan method get
+  public function index_get(){
+    $response = $this->user_model->all_user();
+    $this->response($response);
+  }
+
+  public function login_post(){
+
+    $email    = $this->post('email');
+    $password = $this->post('password');
+
+    $cek = $this->user_model->get_where('t_user', array('email' => $email));
+
+    if ($cek->num_rows() > 0) 
+    {
+        $data = $cek->row();
+
+        if (password_verify($password, $data->password)) 
+        {
+          $response['status']=200;
+          $response['error']=false;
+          $response['message']='Selamat Anda Berhasil Login!';
+          $this->response($response);
+
+        } else {
+          $response['status']=200;
+          $response['error']=false;
+          $response['message']='Password Yang Anda Masukkan Salah!';
+          $this->response($response);
+        } 
+
+    } else {
+        $response['status']=200;
+        $response['error']=false;
+        $response['message']='Email Tidak Terdaftar!';
+        $this->response($response);
+    }
+   
+  }
+
+  // untuk menambah person menaggunakan method post
+  public function tambah_post(){
+    $response = $this->user_model->add_person(
+        $this->post('name'),
+        $this->post('address'),
+        $this->post('phone')
+      );
+    $this->response($response);
+  }
+
+  // update data person menggunakan method put
+  public function update_put(){
+    $response = $this->user_model->update_person(
+        $this->put('id'),
+        $this->put('name'),
+        $this->put('address'),
+        $this->put('phone')
+      );
+    $this->response($response);
+  }
+
+  // hapus data person menggunakan method delete
+  public function delete_delete(){
+    $response = $this->user_model->delete_person(
+        $this->delete('id')
+      );
+    $this->response($response);
+  }
+
+}
+
+?>
---
+++ application/models/User_model.php
@@ -0,0 +1,126 @@
+<?php
+
+// extends class Model
+class User_model extends CI_Model{
+
+  // response jika field ada yang kosong
+  public function empty_response(){
+    $response['status']=502;
+    $response['error']=true;
+    $response['message']='Field tidak boleh kosong';
+    return $response;
+  }
+
+  // function untuk insert data ke tabel t_user
+  public function add_person($name,$address,$phone){
+
+    if(empty($name) || empty($address) || empty($phone)){
+      return $this->empty_response();
+    }else{
+      $data = array(
+        "name"=>$name,
+        "address"=>$address,
+        "phone"=>$phone
+      );
+
+      $insert = $this->db->insert("t_user", $data);
+
+      if($insert){
+        $response['status']=200;
+        $response['error']=false;
+        $response['message']='Data person ditambahkan.';
+        return $response;
+      }else{
+        $response['status']=502;
+        $response['error']=true;
+        $response['message']='Data person gagal ditambahkan.';
+        return $response;
+      }
+    }
+
+  }
+
+  // mengambil semua data person
+  public function all_user(){
+    // $nama = array('name' => 'Dono' );
+
+    // $this->db->where($nama);
+    $all = $this->db->get("t_user")->result();
+    $response['status']=200;
+    $response['error']=false;
+    $response['person']=$all;
+    return $response;
+
+  }
+
+  public function get_where($table = null, $where = null)
+  {
+    $this->db->from($table);
+    $this->db->where($where);
+
+    return $this->db->get();
+  }
+
+  // hapus data person
+  public function delete_person($id){
+
+    if($id == ''){
+      return $this->empty_response();
+    }else{
+      $where = array(
+        "id"=>$id
+      );
+
+      $this->db->where($where);
+      $delete = $this->db->delete("t_user");
+      if($delete){
+        $response['status']=200;
+        $response['error']=false;
+        $response['message']='Data person dihapus.';
+        return $response;
+      }else{
+        $response['status']=502;
+        $response['error']=true;
+        $response['message']='Data person gagal dihapus.';
+        return $response;
+      }
+    }
+
+  }
+
+  // update person
+  public function update_person($id,$name,$address,$phone){
+
+    if($id == '' || empty($name) || empty($address) || empty($phone)){
+      return $this->empty_response();
+    }else{
+      $where = array(
+        "id"=>$id
+      );
+
+      $set = array(
+        "name"=>$name,
+        "address"=>$address,
+        "phone"=>$phone
+      );
+
+      $this->db->where($where);
+      $update = $this->db->update("t_user",$set);
+      if($update){
+        $response['status']=200;
+        $response['error']=false;
+        $response['message']='Data person diubah.';
+        return $response;
+      }else{
+        $response['status']=502;
+        $response['error']=true;
+        $response['message']='Data person gagal diubah.';
+        return $response;
+      }
+    }
+
+  }
+
+}
+
+?>
